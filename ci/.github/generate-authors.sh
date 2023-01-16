70f2956bd3f7a7782425158c4b5595cba8c36098
Atsushi Watanabe <atsushi.w@ieee.org>
2023-01-16 15:47:48 +0900

    Unify shell script style
    Format by shfmt and check format on CI

--- ci/.github/generate-authors.sh
+++ ci/.github/generate-authors.sh
@@ -11,7 +11,10 @@
 
 set -e
 
-SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+SCRIPT_PATH=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
 if [ -z "${AUTHORS_PATH}" ]; then
   AUTHORS_PATH="$GITHUB_WORKSPACE/AUTHORS.txt"
 fi
@@ -31,39 +34,38 @@ EXCLUDED_CONTRIBUTORS+=('John R. Bradley' 'renovate[bot]' 'Renovate Bot' 'Pion B
 
 CONTRIBUTORS=()
 
-shouldBeIncluded () {
-	for i in "${EXCLUDED_CONTRIBUTORS[@]}"; do
-		if [[ $1 =~ "$i" ]]; then
-			return 1
-		fi
-	done
-	return 0
+shouldBeIncluded() {
+  for i in "${EXCLUDED_CONTRIBUTORS[@]}"; do
+    if [[ $1 =~ "$i" ]]; then
+      return 1
+    fi
+  done
+  return 0
 }
-
 
 IFS=$'\n' #Only split on newline
 for CONTRIBUTOR in $(
-	(
-		git log --format='%aN <%aE>'
-		git log --format='%(trailers:key=Co-authored-by)' | sed -n 's/^[^:]*:\s*//p'
-	) | LC_ALL=C.UTF-8 sort -uf
+  (
+    git log --format='%aN <%aE>'
+    git log --format='%(trailers:key=Co-authored-by)' | sed -n 's/^[^:]*:\s*//p'
+  ) | LC_ALL=C.UTF-8 sort -uf
 ); do
-	if shouldBeIncluded ${CONTRIBUTOR}; then
-		CONTRIBUTORS+=("${CONTRIBUTOR}")
-	fi
+  if shouldBeIncluded ${CONTRIBUTOR}; then
+    CONTRIBUTORS+=("${CONTRIBUTOR}")
+  fi
 done
 unset IFS
 
 if [ ${#CONTRIBUTORS[@]} -ne 0 ]; then
-	cat >${AUTHORS_PATH} <<-'EOH'
+  cat <<EOH >${AUTHORS_PATH}
 # Thank you to everyone that made Pion possible. If you are interested in contributing
 # we would love to have you https://github.com/pion/webrtc/wiki/Contributing
 #
 # This file is auto generated, using git to list all individuals contributors.
-# see `.github/generate-authors.sh` for the scripting
+# see $(.github/generate-authors.sh) for the scripting
 EOH
-    for i in "${CONTRIBUTORS[@]}"; do
-	    echo "$i" >> ${AUTHORS_PATH}
-    done
-    exit 0
+  for i in "${CONTRIBUTORS[@]}"; do
+    echo "$i" >>${AUTHORS_PATH}
+  done
+  exit 0
 fi
