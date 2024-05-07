c6c47a7699821e424cd54b368d55646657700867
DavidDiasN <daviddiasnogueira@gmail.com>
2024-05-06 23:30:39 -0400

    Port lint-no-trailing-newline-in-log-messages
    sh -> Go

---
+++ scripts/lint_no_trailing_newline_in_log_messages.go
@@ -0,0 +1,51 @@
+/*
+SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+SPDX-License-Identifier: MIT
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	parentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	logReg, err := regexp.Compile(`\.(Trace|Debug|Info|Warn|Error)f?\("[^"]*\\n"\)?`)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = filepath.Walk(parentDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() || filepath.Ext(path) != ".go" {
+			return err
+		}
+
+		fileContents, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		for _, match := range logReg.FindAll(fileContents, -1) {
+			if !strings.Contains(string(match), "nolint") {
+				return fmt.Errorf("Log format strings should have trailing new-line: %s\n", match)
+				os.Exit(1)
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
