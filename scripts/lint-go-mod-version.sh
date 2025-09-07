20e4a4667a21b976ec0c6de22973694f5c349c09
Joe Turki <git@joeturki.com>
2025-09-08 01:29:37 +0300

    Handle go semver versions

--- scripts/lint-go-mod-version.sh
+++ scripts/lint-go-mod-version.sh
@@ -20,7 +20,11 @@ fi
 GO_MOD_FILE=go.mod
 GO_MOD_VERSION=$(sed -En 's/^go[[:space:]]+([[:digit:].]+)$/\1/p' ${GO_MOD_FILE})
 
-if [[ ${GO_MOD_VERSION} != ${GO_MOD_VERSION_EXPECTED} ]]; then
+# Extract major.minor version from semver
+GO_MOD_VERSION_MAJOR_MINOR=$(echo ${GO_MOD_VERSION} | sed -E 's/^([0-9]+\.[0-9]+).*/\1/')
+GO_MOD_VERSION_EXPECTED_MAJOR_MINOR=$(echo ${GO_MOD_VERSION_EXPECTED} | sed -E 's/^([0-9]+\.[0-9]+).*/\1/')
+
+if [[ ${GO_MOD_VERSION_MAJOR_MINOR} != ${GO_MOD_VERSION_EXPECTED_MAJOR_MINOR} ]]; then
   if [[ -n "${CI}" ]]; then
     GO_MOD_VERSION_LINE=$(sed -n '/^go/=' go.mod)
     echo "::error title=Invalid Go version,file=${GO_MOD_FILE},line=${GO_MOD_VERSION_LINE},::Found ${GO_MOD_VERSION}. Expected ${GO_MOD_VERSION_EXPECTED}"
